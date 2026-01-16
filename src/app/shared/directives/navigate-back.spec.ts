0312dacd5d53d522c6153ba8f37abaf762029c1d
Juan Carlos Altamirano <BAY1803235@dinersclub.com.ec>
2026-01-16 06:31:04 -0500

    feat: directiva para botones para navegar hacia atras

---
+++ src/app/shared/directives/navigate-back.spec.ts
@@ -0,0 +1,8 @@
+import { NavigateBack } from './navigate-back';
+
+describe('NavigateBack', () => {
+  it('should create an instance', () => {
+    const directive = new NavigateBack();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/shared/directives/navigate-back.ts
@@ -0,0 +1,48 @@
+import { Directive, inject, input } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+
+/**
+ * NavigateBack Directive
+ *
+ * This directive enables backward navigation in the browser history.
+ * When applied to a button or clickable element, it automatically
+ * binds a click event that attempts to navigate to the previous
+ * history entry.
+ *
+ * If there is no previous history available (e.g., the page was
+ * opened directly or refreshed), the directive falls back to navigating
+ * to a specified route.
+ *
+ * Usage:
+ * - Attach the directive to a button or clickable element.
+ * - Optionally provide a fallback route using the `fallback` input.
+ *
+ * Example:
+ * <button appNavigateBack [fallback]="'/home'">Back</button>
+ *
+ * Behavior:
+ * - If browser history length > 1 → navigates back using Location service.
+ * - If no history is available and a fallback is defined → navigates to the fallback route.
+ */
+@Directive({
+  standalone: true,
+  selector: '[appNavigateBack]',
+  host: {
+    '(click)': 'onClick()'
+  }
+})
+export class NavigateBack {
+  public readonly fallback = input<string | null>(null);
+
+  private readonly location = inject(Location);
+  private readonly router = inject(Router);
+
+  protected onClick() {
+    if (history.length > 1) {
+      this.location.back();
+    } else if (this.fallback()) {
+      this.router.navigate([this.fallback()!]);
+    }
+  }
+}
